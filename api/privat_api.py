4205f3e6de83fb42a5b16f7906ab0579a5973b80
Yuriy Puris <donovanyuriy@gmail.com>
2019-07-02 23:40:44 +0300

    raise errors | work with dict

--- api/privat_api.py
+++ api/privat_api.py
@@ -21,10 +21,9 @@ class Api(_Api):
 
     def _find_rate(self, response_data, from_currency):
         privat_aliases_map = {840: "USD", 1000: "BTC"}
-
         if from_currency not in privat_aliases_map:
             raise ValueError(f"Invalid from_currency: {from_currency}")
-
+        
         currency_alias = privat_aliases_map[from_currency]
         for e in response_data:
             if e["ccy"] == currency_alias:
