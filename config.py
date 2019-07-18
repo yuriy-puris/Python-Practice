cb49fdc156efa4cdb60b250487e46b234a0bbb7a
Yuriy Puris <ypuris@itera-research.com>
2019-07-18 16:16:48 +0300

    fx

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ DB_NAME = "golden-eye.db"
 
 HTTP_TIMEOUT = 15
 
-IP_LIST = ['127.0.0.1', '127.0.0.10']
+IP_LIST = ['127.0.0.1', '127.0.0.10', "176.115.99.211", "10.41.255.55"]
 
 LOGGING = {
     'version': 1,
@@ -17,6 +17,10 @@ LOGGING = {
         },
     },
     'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        },
         'file': {
             'class': 'logging.FileHandler',
             'formatter': 'default',
